51e2624c0ddffbcb30058dee6bcd746a34824eeb
olele <13237742998@163.com>
2025-07-23 19:13:16 +0800

    scanner: implement scan an integer

--- src/scan.cpp
+++ src/scan.cpp
@@ -7,9 +7,13 @@ class Scanner {
 	Scanner(Data data) {
 		this.data = data;
 	}	
-	
-	int next(void) {
-		int c;
+
+	int chrpos(std::string s, char c) {
+		return s.find(c)
+	}
+
+	char next() {
+		char c;
 
 		if (this.data.putBack) {
 			c = this.data.putBack;
@@ -25,4 +29,34 @@ class Scanner {
 		this.data.inFile.close();
 		return c;
 	}
+
+	void put_back(char c) {
+		this.data.putBack = c;
+	}
+
+	char skip() {
+		char c;
+
+		c = this.next();
+		while (' ' == c || '\t' == c || '\n' == c || '\r' == c || '\f' == c) {
+			c = this.next();
+		}
+		
+		return c;
+	}
+
+	int scan_int(char c) {
+		int k;
+		int val = 0;
+
+		while ((k = this.chrpos("0123456789", c)) >= 0) {
+		val = val * 10 + k;
+		c = this.next();
+		}
+
+		this.put_back(c);
+		return val;
+	}
+
+
 }
