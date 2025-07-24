cac57f5f1cd7c742775fc0d0f6b2638a10db4cd6
olele <13237742998@163.com>
2025-07-24 22:42:11 +0800

    scanner: complete implement the scan function

--- src/scan.cpp
+++ src/scan.cpp
@@ -1,3 +1,6 @@
+#include <cstdio>
+#include <cctype>
+#include <format>
 #include <string>
 #include "data.hpp"
 
@@ -7,6 +10,10 @@ class Scanner {
 	Scanner(Data data) {
 		this.data = data;
 	}	
+
+	Scanner(std::string fileName) {
+		this.data = new Data(fileName);
+	}
 
 	int chrpos(std::string s, char c) {
 		return s.find(c)
@@ -50,13 +57,44 @@ class Scanner {
 		int val = 0;
 
 		while ((k = this.chrpos("0123456789", c)) >= 0) {
-		val = val * 10 + k;
-		c = this.next();
+			val = val * 10 + k;
+			c = this.next();
 		}
 
 		this.put_back(c);
 		return val;
 	}
 
+	int scan(struct Token t) {
+		char c;
 
+		c = skip();
+
+		switch (c) {
+			case EOF:
+				return 0;
+			case '+':
+				this.data.token.tokenType = Plus;
+				break;
+			case '-':
+				this.data.token.tokenType = Minus;
+				break;
+			case '*':
+				this.data.token.tokenType = Star;
+				break;
+			case '/':
+				this.data.token.tokenType = Slash;
+				break;
+			default:
+				if (std::isdigit(c)){
+					this.data.token.intValue = scan_int(c);
+					this.data.token.tokenType = IntLit;
+					break;
+				}
+				
+				throw std::format("Unrecongnised character {} on line {}\n", c, this.data.line);
+		}
+
+		return 1;
+	}
 }
