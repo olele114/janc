4b7639c183f28ed4d73b6d992bd7cdc17ac7b3da
olele114 <130834155+olele114@users.noreply.github.com>
2025-07-24 23:19:32 +0800

    optimize: main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,49 @@
 #include "data.hpp"
-#include "scan.cpp"
+#include "scanner.hpp"
 #include <vector>
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-static void init(Scanner s) {
-	s.data.line = 1;
-	s.data.putBack = '\n';
+static void init(Scanner& s) {
+    s.data.line = 1;
+    s.data.putBack = '\n';
 }
 
-static void usage(string prog) {
-	throw runtime_error("Usage: " + prog + "infile\n");
+static void usage(const string& prog) {
+    cerr << "Usage: " << prog << " infile\n";
+    exit(1);
 }
 
-vector<string> tokStr = {"+", "-", "*", "/", "intLit"};
+vector<string> tokStr = {"Plus", "Minus", "Star", "Slash", "IntLit"};
 
-static void scan_file(Scanner s) {
-	while (s.scan()) {
-		cout << format("Token {}", tokStr.at(s.data.token.type));
-		if (s.data.token.type == TokenType::IntLit) {
-			cout << format(", value {}", s.data.token.intValue) << endl;
-		}
-	}
+static void scan_file(Scanner& s) {
+    while (s.scan()) {
+        cout << "Token " << tokStr[static_cast<int>(s.data.token.type)];
+        
+        if (s.data.token.type == TokenType::IntLit) {
+            cout << ", value " << s.data.token.intValue;
+        }
+        cout << endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		usage(argv[0]);
-	}
+    try {
+        if (argc != 2) {
+            usage(argv[0]);
+        }
 
-	Data d(argv[1]);
-	Scanner s(d);
+        Data d(argv[1]);
+        Scanner s(d);
 
-	init(s);
-
-	scan_file(s);
-	return 0;
+        init(s);
+        scan_file(s);
+        
+        return 0;
+    } catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
 }
